e77067e7e600ea1590abe3c71d013062e7b5f172
Ana Vicovac <anavicovac@gmail.com>
2026-03-23 22:32:30 +0100

    api mvp (generate maze and solve with a-star)

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject clojure-project "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [ring/ring-core "1.15.3"]
+                 [ring/ring-jetty-adapter "1.15.3"]
+                 [ring/ring-json "0.5.1"]
+                 [compojure "1.7.2"]]
 
   :profiles
   {:dev {:dependencies [[midje "1.10.10"]]}
-   :uberjar {:aot :all}})
+   :uberjar {:aot :all}}
+  
+  :main server)
---
+++ src/server.clj
@@ -0,0 +1,44 @@
+(ns server
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [ring.util.response :refer [response]]
+            [maze :as m]
+            [maze-solver :as s]))
+
+(defroutes app-routes
+  (GET "/" []
+    (response {:mess "Server is listening"}))
+  
+  (POST "/generate" request
+    (let [body (:body request)
+          rows (:rows body 10)
+          cols (:cols body 10)
+          maze-data (m/generate-maze rows cols)]
+      (response maze-data)
+      ))
+  
+  (POST "/solve-a-star" request
+    (let [body (:body request)
+          grid (:grid body)
+          start (:start-cell body)
+          end (:end-cell body)
+          
+          solution (s/solve-a-star grid start end)]
+      
+      (response solution)))
+  
+  (route/not-found (response {:err "Route not found"})))
+
+;;middleware (JSON to map and map to JSON)
+(def app
+  (-> app-routes
+      (wrap-json-body {:keywords? true})
+      (wrap-json-response)))
+
+(defn -main []
+  (println "Server is running....")
+  (run-jetty app {:port 3000 :join? false}))
+
+;;Jetty Eclipse je Java HTTP web server
